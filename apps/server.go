905ffcaf22fb06dfc677e45913b937a428da9411
xiaohuzi <1269146988@qq.com>
2022-08-09 16:18:40 +0800

    2.0.2 1. 封装处理光标位置的函数 2. 搜索时使用默认光标，并且动态调整光标位置，之前打中文时会显示光标位置在左上角第一行，看起来很怪异。现在已经修复

--- apps/server.go
+++ apps/server.go
@@ -9,11 +9,12 @@ import (
 )
 
 var (
-	H          = flag.Bool("help", false, "显示帮助信息")
-	S          = flag.Bool("s", false, "载入ssh配置 config '~/.ssh/config'")
-	C          = flag.String("c", configName, "服务器配置文件名")
-	configName = "go_ssh.yaml"
-	logs       = GetLogger()
+	H            = flag.Bool("help", false, "显示帮助信息")
+	S            = flag.Bool("s", false, "载入ssh配置 config '~/.ssh/config'")
+	C            = flag.String("c", configName, "服务器配置文件名")
+	configName   = "go_ssh.yaml"
+	logs         = GetLogger()
+	ClearContent = "\033[K" // 清除从光标到行尾的内容
 )
 
 func Run() {
@@ -102,7 +103,7 @@ func getServers(trees []*Node, i int) []string {
 	noResult := true
 	for index, item := range trees {
 		if item == nil {
-			content = append(content, "\033[K")
+			content = append(content, ClearContent)
 		} else if index == i {
 			noResult = false
 			content = append(content, item.Str(true))
@@ -113,8 +114,7 @@ func getServers(trees []*Node, i int) []string {
 	}
 	if noResult && len(trees) != 0 {
 		// 说明搜索没搜到任何服务器
-		content[1] = "  🍵 未找匹配到数据\033[K"
-		//content = append(content[1:], "未找到任何服务器\033[K")
+		content[1] = "  🍵 未找匹配到数据" + ClearContent
 	}
 	return content
 }
@@ -129,11 +129,52 @@ type ServerInfo struct {
 	height        int // 内容的高度
 }
 
+// \033[0m 关闭所有属性
+//\033[1m 设置高亮度
+//\033[4m 下划线
+// \033[5m 闪烁
+//\033[7m 反显
+//\033[8m 消隐
+//\033[30m — \033[37m 设置前景色
+//\033[40m — \033[47m 设置背景色
+//\033[nA 光标上移n行
+//\033[nB 光标下移n行
+//\033[nC 光标右移n行
+//\033[nD 光标左移n行
+//\033[y;xH设置光标位置
+//\033[2J 清屏
+//\033[K 清除从光标到行尾的内容
+//\033[s 保存光标位置
+//\033[u 恢复光标位置
+//\033[?25l 隐藏光标
+//\033[?25h 显示光标
 // HideCursor 隐藏光标
 func HideCursor() {
 	fmt.Printf("\033[?25l")
 }
 
+func ShowCursor() {
+	fmt.Printf("\033[?25h")
+}
+
+func MoveCursorUP(y int) {
+	// 相对位置移动，而不是按照整个屏幕定位
+	// \033[nA 光标上移n行
+	// \033[nC 光标右移n行
+	fmt.Printf("\033[%dA", y)
+}
+func MoveCursorRight(x int) {
+	// 往右移动光标
+	fmt.Printf("\033[%dC", x)
+}
+func SaveCursor() {
+	fmt.Printf("\033[s")
+}
+
+func RecoveryCursor() {
+	fmt.Printf("\033[u")
+
+}
 func (s *ServerInfo) getTips() []string {
 	// 根据搜索内容匹配服务器信息
 	if len(s.SearchContent) != 0 && s.searchContent != s.SearchContent {
@@ -158,8 +199,9 @@ func (s *ServerInfo) getTips() []string {
 
 	}
 	s.searchContent = s.SearchContent
+	// 根据输入的内容计算光标移动的偏差
 
-	return []string{fmt.Sprintf("🔍 输入自动搜索：%s█\033[K", s.SearchContent), Green("✨ 请选择要连接的服务器：")}
+	return []string{fmt.Sprintf("🔍 输入自动搜索：%s"+ClearContent, s.SearchContent), Green("✨ 请选择要连接的服务器：")}
 }
 
 func (s *ServerInfo) getContent() []string {
@@ -176,16 +218,17 @@ func (s *ServerInfo) Draw() {
 	if height > s.height {
 		s.height = height
 	}
-
+	RecoveryCursor()
 	for _, s := range content {
 		fmt.Println(s)
 	}
-	fmt.Printf("\033[%dA", s.height)
+	// 计算第一列输入的提示语句和已输入的内容的长度
+	MoveCursorUP(s.height)
+	MoveCursorRight(ZhLen(content[0]) - 1)
 }
 
 func NewServerInfo(trees []*Node) *ServerInfo {
 	initLength(trees)
-	HideCursor()
 	return &ServerInfo{
 		Nodes:  trees,
 		nodes:  trees,
@@ -194,6 +237,7 @@ func NewServerInfo(trees []*Node) *ServerInfo {
 }
 
 func choose(trees []*Node) *Node {
+	SaveCursor()
 	serverInfo := NewServerInfo(trees)
 	serverInfo.Draw()
 	// 绘制之后，开始监听键盘
@@ -255,14 +299,10 @@ func (s *ServerInfo) deleteSearchContent() {
 }
 func (s *ServerInfo) clear() {
 	for i := 0; i < s.height; i++ {
-		fmt.Println("\033[K")
+		fmt.Println(ClearContent)
 	}
-	fmt.Printf("\033[%dA", s.height)
+	MoveCursorUP(s.height)
 	ShowCursor()
-}
-
-func ShowCursor() {
-	fmt.Printf("\033[?25h")
 }
 
 // 处理键盘除字母键以外的按键
